1545211757cbf150b17b2f3cd567219695420829
majpark21 <marqui.thomas@yahoo.fr>
2018-02-22 13:46:27 +0100

    overlay_text works with list of coords, custom font and documented

--- overlay_text.py
+++ overlay_text.py
@@ -1,11 +1,22 @@
-def overlay_txt(imfile, coord, text, output=None, font=None, color=None, show=False):
-    """"Read image file and add text at specified position.
+def overlay_text(imfile, coord, text, output=None, font=None, color=None, show=False):
+    """"Read image file, add text at specified positions and save.
 
     Args:
         imfile (str): Path to the image file.
-        coord (2-tuple): Coordinates (x,y) where text is written.
-        text (str): Text to be written.
+        coord (list of 2-tuple): Coordinates (x,y) where text is written.
+        text (list of str): Text to be written.
         output(str, optional): Path to save the image with overlayed text. Defaults to imfile with 'ovl_' prefix.
+        font (ImageFont object, optional): Font of text. Defaults to arial, 12pt.
+        color (n-tuple, optional): Color of text. Should have same length as the number of channels in image. Defaults
+        to black.
+        show (bool, optional): Whether to display the annotated image. Defaults to False.
+    Returns:
+        None
+    Examples:
+        # Custom font (tested on Windows, see doc if error)
+        myfont= ImageFont.truetype(font='calibri.ttf', size=20)
+        overlay_text('path/to/image.jpg', [(40,10), (80,120)], ['1st coords', '2nd coords'], font=myfont)
+
     """
     from PIL import Image, ImageDraw, ImageFont
     # Read image file and create drawing object
@@ -21,9 +32,8 @@ def overlay_txt(imfile, coord, text, output=None, font=None, color=None, show=Fa
         default_col = {'L':0, 'RGB':(0,0,0)}
         color = default_col[im.mode]
     # Add text and save
-    imdraw.text(coord, text, font = font, fill = color)
+    for xy, txt in zip(coord, text):
+        imdraw.text(xy, txt, font = font, fill = color)
     im.save(output)
     if show:
         im.show()
-
-overlay_label('temp_astro.jpg', (40,40), 'blabla')
