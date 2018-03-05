d97a990a33490b811f5b156b350911f044662a7e
majpark21 <marqui.thomas@yahoo.fr>
2018-03-05 14:19:42 +0100

    Stop and watch, script format

--- script_stop_and_watch.R
+++ script_stop_and_watch.R
@@ -1,33 +1,41 @@
-library(tcltk)
-suppressMessages(library(data.table))
-library(ggplot2)
+#rm(list=ls())
+# Path to file to read
+filetoread = "mod_tCoursesSelected.csv"
+col.uniqID = "myID"
+col.time = "RealTime"
+col.whatoplot = c("objNuc_Intensity_MeanIntensity_imNucCorrBg", "objCell_AreaShape_Area", "objCell_AreaShape_EulerNumber")
 
 # At which trajectory to start, useul if you stopped in the middle of a dataset and want to 
 # come back to it
 i <- 1L
 # Increase if GUI appears before the plot
 sleep.time <- 0.75
+# Number of trajectories to plot (selected at random, lower to speed up plotting)
+in.ntraj <- 20
 
 
+# ----------------------------
+
+library(tcltk)
+suppressMessages(library(data.table))
+library(ggplot2)
 # Read Input
-dt <- fread(input = "tCoursesSelected.csv")
-dt <- dt[Well==4, .(Well, Image_Metadata_Site, objNuc_TrackObjects_Label,
-                    RealTime, objNuc_Intensity_MeanIntensity_imErkCorrOrig,
-                    objCyto_Intensity_MeanIntensity_imErkCorrOrig)]
-setnames(dt, c("Image_Metadata_Site", "objNuc_TrackObjects_Label",
-               "objNuc_Intensity_MeanIntensity_imErkCorrOrig",
-               "objCyto_Intensity_MeanIntensity_imErkCorrOrig"),
-             c("Site", "Label", "Nuc_ERK", "Cyto_ERK"))
-dt[, uniqID := paste(Well, Site, Label, sep="_")]
-dt[, Ratio_ERK := Cyto_ERK / Nuc_ERK]
+dt <- fread(input = filetoread)
+# Chop unuse columns for memor efficiency and melt on features to plot
+col.tokeep <- c(col.uniqID, col.time, col.whatoplot)
+dt <- dt[, col.tokeep, with=FALSE]
+dt <- melt(dt, measure.vars = col.whatoplot)
 
-whole.plot <- ggplot(dt, aes(x=RealTime, y=Ratio_ERK)) +
-  geom_line(aes(group=Label)) +
+# Select some random trajectories to always plot
+traj.to.plot <- sample(unlist(unique(dt[, col.uniqID, with=FALSE])), in.ntraj, FALSE)
+whole.plot <- ggplot(dt[get(col.uniqID) %in% traj.to.plot], aes(x=get(col.time), y=value)) +
+  geom_line(aes_string(group=col.uniqID)) +
   stat_summary(fun.y=mean, geom="line", colour = "blue", size = 1.5) + 
+  facet_wrap(~variable, scales = "free") +
   theme_bw()
 
 # outlier table
-dt.out <- data.table(uniqID=unique(dt$uniqID), is.outlier = ".")
+dt.out <- data.table(unique(dt[, col.uniqID, with=FALSE]), is.outlier = ".")
 
 
 # Window definition
@@ -39,22 +47,22 @@ myWindow <- function(){
   butOUT <- ttkbutton(win, text = "Outlier", width = -6,
                       command = function(){set(dt.out, i, 2L, "yes"); tkdestroy(win)})
   butnotOUT<- ttkbutton(win, text = "NOT an Outlier", width = -6,
-                      command = function(){set(dt.out, i, 2L, "no"); tkdestroy(win)})
+                        command = function(){set(dt.out, i, 2L, "no"); tkdestroy(win)})
   # -2 since the while loop is -1
   butPREV<- ttkbutton(win, text = "Back to previous", width = -6,
                       command = function(){i <<- i-2L ; tkdestroy(win)})
   butSAVEQUIT <- ttkbutton(win, text = "Save and quit", width = -6,
-                      command = function(){
-                        filename <- tclvalue(tkgetSaveFile())
-                        if (!nchar(filename)) {
-                          tkmessageBox(message = "No file was selected!")
-                        } else {
-                          tkmessageBox(message = paste("The file selected was", filename))
-                        }
-                       write.csv(x = dt.out, file = paste0(filename, ".csv"), quote = FALSE)
-                       tkdestroy(win)
-                       stoploop <<- TRUE
-                      })
+                           command = function(){
+                             filename <- tclvalue(tkgetSaveFile())
+                             if (!nchar(filename)) {
+                               tkmessageBox(message = "No file was selected!")
+                             } else {
+                               tkmessageBox(message = paste("The file selected was", filename))
+                             }
+                             write.csv(x = dt.out, file = paste0(filename, ".csv"), quote = FALSE)
+                             tkdestroy(win)
+                             stoploop <<- TRUE
+                           })
   tkgrid(butOUT, butnotOUT, butPREV, butSAVEQUIT)
   tkwait.window(win)
 }
@@ -83,7 +91,7 @@ stoploop <- FALSE
 while(i <= 3L){
   print(i)
   plot(whole.plot +
-    geom_line(data = dt[uniqID==dt.out[i, uniqID]], aes(x=RealTime, y=Ratio_ERK), col = 'red', size = 2))
+         geom_line(data = dt[get(col.uniqID)==dt.out[i, get(col.uniqID)]], aes(x=get(col.time), y=value), col = 'red', size = 2))
   Sys.sleep(sleep.time)
   myWindow()
   if(stoploop) break
