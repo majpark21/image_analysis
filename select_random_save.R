0f529c13b848d280be3a0c0b5a0ae67bed868866
majpark21 <marqui.thomas@yahoo.fr>
2018-03-05 17:38:20 +0100

    select_random_save can use local variables

--- select_random_save.R
+++ select_random_save.R
@@ -1,6 +1,7 @@
 select_random_save <- function(in.file, ntraj, col.uniqID, col.time, col.whatokeep, out.file=NULL){
   require(data.table)
-  dt <- fread(in.file)
+  if(is.character(in.file)) dt <- fread(in.file)
+  else dt <- in.file
   trajs <- sample(unlist(unique(dt[, col.uniqID, with=FALSE])), size=ntraj, replace=FALSE)
   dt <- dt[get(col.uniqID) %in% trajs, c(col.uniqID, col.time, col.whatokeep), with=FALSE]
   if(!is.null(out.file)) write.csv(dt, out.file, quote = FALSE, row.names = FALSE)
